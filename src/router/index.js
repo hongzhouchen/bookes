788bc7e6f01a45e47a8559a8ecedf3a93acf0b1f
hongzhouchen <798838667@qq.com>
2018-09-25 17:27:00 +0800

    swipeer

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Me from '@/views/me/Me'
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes: [
     {
       path: '/',
--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,22 +1,47 @@
 <template>
 
   <div class="contianer">
-       {{name}}
+    {{name}}
+    <!--首页轮播-->
+    <section class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">slider1</div>
+        <div class="swiper-slide">slider2</div>
+        <div class="swiper-slide">slider3</div>
+      </div>
+      <!--分页点-->
+      <div class="swiper-pagination"></div>
+    </section>
   </div>
 
 
 </template>
 
-<style scoped >
+<style>
+
 
 </style>
 <script>
-    /**/
-    export default {
-        data() {
-            return {
-                name: 'Charles'
-            }
+  import Swiper from "swiper";
+  import "swiper/dist/css/swiper.min.css"
+
+
+  /**/
+  export default {
+    mounted() {
+      /*创建Vue 的实例对象 */
+      var mySwiper = new Swiper(".swiper-container", {
+        loop: true,
+        autoplay:true,
+        pagination: {
+          el: '.swiper-pagination',
         }
+      });
+    },
+    data() {
+      return {
+        name: 'Charles'
+      }
     }
+  }
 </script>
